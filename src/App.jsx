1e86d91286e6a716fb869ed478298807262525e1
SetUp <abdulmumeenibroheem51@gmail.com>
2023-09-28 19:58:57 +0100

    project setup-initialized completed..

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>Welcome to the recording App</div>;
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-  popup: "./src/popup.jsx",
+  entry: "./src/popup.jsx",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
@@ -10,14 +11,23 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: "node_modules/",
+        exclude: "/node_modules/",
         use: {
           loader: "babel-loader",
-          option: {
+          options: {
             presets: ["@babel/preset-env", "@babel/preset-react"],
           },
         },
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/popup.html",
+      filename: "popup.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "public" }],
+    }),
+  ],
 };
